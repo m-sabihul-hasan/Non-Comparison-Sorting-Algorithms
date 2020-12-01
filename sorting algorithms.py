be2ceb1b49540cab4902e8b56108aa7f8491d2a8
m-sabihul-hasan <54771392+m-sabihul-hasan@users.noreply.github.com>
2020-12-01 22:30:07 +0500

    Update sorting algorithms.py

--- sorting algorithms.py
+++ sorting algorithms.py
@@ -102,18 +102,18 @@ def plot_graph(N, start_time):
     r_time = []
     b_time = []
     for i in range(N):
-        # lst = [random.randint(0, 100) for x in range(N)]
-        lst = [random.random() for x in range(N)]
+        lst = [random.randint(0, 100) for x in range(N)]
+        # lst = [random.random() for x in range(N)]
         x_lst.append(i)
-        # count_sort(lst)
-        # c_time.append(round(time.time() - start_time, 2))
+        count_sort(lst)
+        c_time.append(round(time.time() - start_time, 2))
         # radix_sort(lst)
         # r_time.append(round(time.time() - start_time, 2))
-        bucket_sort(lst)
-        b_time.append(round(time.time() - start_time, 2))
-    # plt.plot(x_lst, c_time, label="O(n+k)")
+        # bucket_sort(lst)
+        # b_time.append(round(time.time() - start_time, 2))
+    plt.plot(x_lst, c_time, label="O(n+k)")
     # plt.plot(x_lst, r_time, label="O(d(n+k))")
-    plt.plot(x_lst, b_time, label = "O(n+k)")
+    # plt.plot(x_lst, b_time, label = "O(n+k)")
     plt.xlabel("N")
     plt.ylabel("Time")
     # plt.title("Time Complexity for each of the sorting algorithm")
@@ -125,9 +125,9 @@ N = 1000
 
 plot_graph(N, start_time)
 
-# lst = [-7, -12, -3, 5, 1, 5, -3, 5]
-# print("Sorted list = ", count_sort(lst))
-# lst = [2, 44, 98, 121, 0, 0, 3, 2]
-# print("Sorted list = ", radix_sort(lst))
+lst = [-7, -12, -3, 5, 1, 5, -3, 5]
+print("Sorted list = ", count_sort(lst))
+lst = [2, 44, 98, 121, 0, 0, 3, 2]
+print("Sorted list = ", radix_sort(lst))
 lst = [.42, .32, .33, .52, .37, .47, .51]
 print("Sorted list = ", bucket_sort(lst))
